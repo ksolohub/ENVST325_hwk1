526867b0edf38e52f7153e1396eb90782e9c293f
ksolohub <ksolohub@hamilton.edu>
2022-01-31 00:42:51 +0000

    description of changes

--- Activity_1.R
+++ Activity_1.R
@@ -1,1 +1,30 @@
 # activity & homework 1
+
+########### In class prompt----
+#prompt 1
+
+SnowDepth <- c(2.5, 3, 5, 4.5)
+
+SnowDepth*2.54
+
+SnowDepthCM <- c(6.35, 7.62, 12.70, 11.43)
+
+#Prompt 2
+peaknames <- c("Haystack","Skylight","Dix","Peak")
+highpeaks <- data.frame (name = peaknames,
+                        Elevation_ft= c(4960, 4926, 4857, 4840), 
+                        Ascent_ft=c(3570, 4265, 2800, 4178),
+                        length_mi= c(17.8, 17.9, 13.2, 16))
+
+######HW1-----
+#question 1
+highpeaks [,2]*0.3048
+
+#question 2 
+#see google doc
+
+#question 3 
+LongestRT <- highpeaks [2,]
+
+
+
